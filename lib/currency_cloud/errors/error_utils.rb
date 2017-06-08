2c7c8dc1fa04a8b6fffe85997087d509d591b08a
Dirk Zittersteyn <dirk@hackerone.com>
2017-06-08 10:38:10 +0200

    Redact sensitive information from error messages

--- lib/currency_cloud/errors/error_utils.rb
+++ lib/currency_cloud/errors/error_utils.rb
@@ -10,5 +10,23 @@ module CurrencyCloud
                        end
       "#{base}#{implementation}"
     end
+
+    REDACTED_PARAMS = [:api_key, :login_id, :token].freeze
+
+    def redacted_params(params)
+      redacted = params.dup
+
+      REDACTED_PARAMS.each do |param|
+        if redacted.key? param.to_sym
+          redacted[param.to_sym] = 'REDACTED'
+        end
+
+        if redacted.key? param.to_s
+          redacted[param.to_s] = 'REDACTED'
+        end
+      end
+
+      redacted
+    end
   end
 end
