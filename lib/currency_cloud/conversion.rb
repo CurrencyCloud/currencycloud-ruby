382fa7ccab06eb112f4fbea3b1fd51c6bdc1db39
Francesco Boffa <fra.boffa@gmail.com>
2018-06-13 10:33:56 +0100

    Lints

--- lib/currency_cloud/conversion.rb
+++ lib/currency_cloud/conversion.rb
@@ -6,17 +6,17 @@ module CurrencyCloud
     actions :create, :retrieve, :find
 
     def self.cancel(params)
-      attrs = client.post("#{params[:conversion_id]}/cancel", params.delete_if{|key, _value| key == :conversion_id})
+      attrs = client.post("#{params[:conversion_id]}/cancel", params.delete_if { |key, _value| key == :conversion_id })
       new(attrs)
     end
 
     def self.date_change(params)
-      attrs = client.post("#{params[:conversion_id]}/date_change", params.delete_if{|key, _value| key == :conversion_id})
+      attrs = client.post("#{params[:conversion_id]}/date_change", params.delete_if { |key, _value| key == :conversion_id })
       new(attrs)
     end
 
     def self.split(params)
-      attrs = client.post("#{params[:conversion_id]}/split", params.delete_if{|key, _value| key == :conversion_id})
+      attrs = client.post("#{params[:conversion_id]}/split", params.delete_if { |key, _value| key == :conversion_id })
       new(attrs)
     end
   end
