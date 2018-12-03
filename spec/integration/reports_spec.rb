1d90e5ba6fdcb585c99710b77c4354e54a253d56
cpacalevcc <corneliu.pacalev@currencycloud.com>
2018-12-03 15:50:18 +0200

    [TCC-15475] add retrieve report request

--- spec/integration/reports_spec.rb
+++ spec/integration/reports_spec.rb
@@ -73,4 +73,24 @@ describe 'Reports', vcr: true do
     expect(report_requests_result.report_requests[1]['created_at']).to eql('2018-10-26T07:21:32+00:00')
     expect(report_requests_result.report_requests[1]['updated_at']).to eql('2018-10-26T07:21:36+00:00')
   end
+
+  it "can #retrieve_report_request" do
+    report_request = CurrencyCloud::Reports.retrieve_report_request('075ce584-b977-4538-a524-16b759277d66')
+
+    expect(report_request.id).to eql('075ce584-b977-4538-a524-16b759277d66')
+    expect(report_request.short_reference).to eql('RP-5279826-KZJHNX')
+    expect(report_request.description).to eql(nil)
+    expect(report_request.search_params['buy_currency']).to eql('EUR')
+    expect(report_request.search_params['sell_currency']).to eql('GBP')
+    expect(report_request.search_params['scope']).to eql('own')
+    expect(report_request.report_type).to eql('conversion')
+    expect(report_request.status).to eql('completed')
+    expect(report_request.failure_reason).to eql(nil)
+    expect(report_request.expiration_date).to eql('2018-10-18T00:00:00+00:00')
+    expect(report_request.report_url).to eql('https://ccycloud-reports-prod-demo1-customer-reporting.s3.eu-west-1.amazonaws.com/customer_reporting/075ce584-b977-4538-a524-16b759277d66/conversion_report_1610201808101539677748.csv?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=Acredentialsabc&X-Amz-Date=20181203T081009Z&X-Amz-Expires=172800&X-Amz-SignedHeaders=host&X-Amz-Security-Token=tokenabc&X-Amz-Signature=signatureabc')
+    expect(report_request.account_id).to eql('bf5b1007-b364-43cc-b3d6-9f2d1be75297')
+    expect(report_request.contact_id).to eql('ba33d76a-4a7f-4cb3-afa8-5678d5bc712a')
+    expect(report_request.created_at).to eql('2018-10-16T08:15:46+00:00')
+    expect(report_request.updated_at).to eql('2018-10-16T08:15:48+00:00')
+  end
 end
