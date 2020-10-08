1278f2e90c87a5838c5ae9b372852204a4309646
jonathancouchman <jonathan.couchman@currencycloud.com>
2020-10-08 11:19:37 +0100

    Release 0.17.7

--- lib/currency_cloud/version.rb
+++ lib/currency_cloud/version.rb
@@ -1,6 +1,6 @@
 # Separate Gem version from 'API version'
 
 module CurrencyCloud
-  API_VERSION = 'v2'.freeze # API Version
-  VERSION = '0.17.7'.freeze # Gem Version
+  API_VERSION = "v2".freeze # API Version
+  VERSION = "0.17.7".freeze # Gem Version
 end
