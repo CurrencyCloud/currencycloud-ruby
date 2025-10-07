0bf19ad4729a6d22396e8fc667ed368209d12e47
libsdk-gh <libsdk@visa.com>
2025-10-07 09:56:24 +0000

    Release 3.0.0

--- lib/currency_cloud/actions/save.rb
+++ lib/currency_cloud/actions/save.rb
@@ -5,7 +5,7 @@ module CurrencyCloud
         return self if changed_attributes.empty?
 
         params = attributes.select { |attr| changed_attributes.include?(attr) }
-        attrs = client.post(id, params)
+        attrs = client.post(id, **params)
         changed_attributes.clear
         self.attributes = attrs
         self
